ea04c6eed42a084939f0850fcf4626b45d10a870
Seth Delorme <delorme.s.m@gmail.com>
2021-02-22 20:22:45 -0600

    adds namespace route to display first Contact from db

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,6 @@
+class Api::ContactsController < ApplicationController
+  def new_contact
+    @contact1 = Contact.first
+    render 'new_contact.json.jb'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace :api do
+    get "/contacts" => "contacts#new_contact"
+  end
 end
